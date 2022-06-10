968fc24c8093ced76931a8fd822f9852643c742c
BP201 <bp201@code.local>
2022-06-10 18:44:45 +0400

    task

---
+++ WebApplication1/WebApplication1/Models/ProductCategories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ProductCategories
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Products.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int DiscountPercent { get; set; }
+        public string Description { get; set; }
+        public bool IsInStock { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateDAt { get; set; }
+        public bool IsNew { get; set; }
+    }
+}
